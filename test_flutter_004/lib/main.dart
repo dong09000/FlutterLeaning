190fba5ceed6319b323261689d1c803a343f9756
dongyuefeng <dongyuefeng@100tal.com>
2019-04-25 16:42:56 +0800

    尝试动态修改线的颜色，失败

--- test_flutter_004/lib/main.dart
+++ test_flutter_004/lib/main.dart
@@ -20,8 +20,8 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      //home: MyHomePage(title: 'Flutter Demo Home Page'),
-      home: Signature(),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      //home: Signature(),
     );
   }
 }
@@ -45,81 +45,45 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  Color lineColor = Colors.red;
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      lineColor = Colors.blue;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+        
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
+      child: Signature(lineColor: lineColor,),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        child: Icon(Icons.track_changes),
+        tooltip: 'hahaha',
+      ),
     );
   }
 }
 
 
 class SignaturePainter extends CustomPainter {
-  SignaturePainter(this.points);
+  SignaturePainter(this.points, this.lineColor);
 
   final List<Offset> points;
+  final Color lineColor;
 
   void paint(Canvas canvas, Size size) {
     var paint = Paint()
-    ..color = Colors.red
+    ..color = lineColor
     ..strokeCap = StrokeCap.round
     ..strokeWidth = 5.0;
 
@@ -134,10 +98,16 @@ class SignaturePainter extends CustomPainter {
 }
 
 class Signature extends StatefulWidget {
-  SignatureState createState() => SignatureState();
+  Signature({Key key, this.lineColor}) : super(key: key);
+
+  final Color lineColor;
+
+  SignatureState createState() => SignatureState(lineColor);
 }
 
 class SignatureState extends State<Signature> {
+  SignatureState(this.lineColor);
+  final Color lineColor;
   List<Offset> _points = <Offset>[];
 
   Widget build(BuildContext context) {
@@ -150,7 +120,7 @@ class SignatureState extends State<Signature> {
         });
       },
       onPanEnd: (DragEndDetails details) => _points.add(null),
-      child: CustomPaint(painter: SignaturePainter(_points), size: Size.infinite,),
+      child: CustomPaint(painter: SignaturePainter(_points, lineColor), size: Size.infinite,),
     );
   }
 }
